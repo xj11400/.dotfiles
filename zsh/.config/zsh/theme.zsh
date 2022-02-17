886ebc960d20b8c10aefbcd22e7123a3bde4c328
XJ Hsu <xj11400@gmail.com>
2022-02-17 17:29:52 +0800

    <zsh> add snow-blocks theme

--- zsh/.config/zsh/theme.zsh
+++ zsh/.config/zsh/theme.zsh
@@ -8,6 +8,7 @@ source ~/.config/zsh/theme/powerlevel10k/powerlevel10k/powerlevel10k.zsh-theme
 
 # use robbyrussell theme
 #source ~/.config/zsh/theme/powerlevel10k/powerlevel10k/config/p10k-robbyrussell.zsh
+#source ~/.config/zsh/theme/snow_blocks/config/p10k-snowblocks.zsh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.config/zsh/theme/powerlevel10k/.p10k.zsh ]] || source ~/.config/zsh/theme/powerlevel10k/.p10k.zsh
