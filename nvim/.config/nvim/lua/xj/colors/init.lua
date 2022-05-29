8b14873afca5f65463c15f7bf2edb6c6bdae059e
XJ Hsu <xj11400@gmail.com>
2022-05-30 03:10:45 +0800

    <nvim> init colors

---
+++ nvim/.config/nvim/lua/xj/colors/init.lua
@@ -0,0 +1,15 @@
+-- 
+-- local colorscheme = "z"
+-- 
+-- local status_ok, _ = pcall(vim.cmd,"colorscheme "..colorscheme)
+-- if not status_ok then
+--     vim.notify("colorscheme " .. colorscheme .. "not found!")
+--     return
+-- end
+
+vim.opt.termguicolors = true
+
+-- local status_ok, theme = pcall(require,"xj.colors."..colortheme)
+-- pcall(require,"xj.colors."..colortheme)
+
+---------------------------------------------NvChad
