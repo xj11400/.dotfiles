eb4682b132966702e3b3665b8a75ee7edaee8f36
XJ Hsu <xj11400@gmail.com>
2022-06-22 02:38:11 +0800

    <zsh> zi

--- zsh/.config/zsh/plugin/zpm/zi/zi.zsh
+++ zsh/.config/zsh/plugin/zpm/zi/zi.zsh
@@ -1,49 +1,68 @@
 #
 x_use "z-shell/zi" $X_ZSH_DATA_HOME/zpm zi/bin
 
+ZI_HOME="${X_ZSH_DATA_HOME}/zpm/zi"
+
 # initial ZI's hash definition
-declare -A ZI
+declare -gHA ZI
 ZI[BIN_DIR]="$X_ZSH_DATA_HOME/zpm/zi/bin"
 ZI[HOME_DIR]="$X_ZSH_DATA_HOME/zpm/zi"
 
-#ZPFS=$ZI_HOME/polaris
-#ZI[BIN_DIR]=$ZI_HOME/bin
-#ZI[HOME_DIR]="$ZI_HOME"
-#ZI[PLUGINS_DIR]=$ZINIT_HOME/plugins
-#ZI[COMPLETIONS_DIR]=$ZINIT_HOME/completions
-#ZI[SNIPPETS_DIR]=$ZINIT_HOME/snippets
-#
-#ZI[ZMODULES_DIR]=/opt/zsh/zi/zmodules
-#ZI[ZCOMPDUMP_PATH]=$XDG_CACHE_HOME/zcompdump
+ZPFS="$ZI_HOME/polaris"
+ZI[PLUGINS_DIR]="$ZI_HOME/plugins"
+ZI[COMPLETIONS_DIR]="$ZI_HOME/completions"
+ZI[SNIPPETS_DIR]="$ZI_HOME/snippets"
+ZI[ZMODULES_DIR]="$ZI_HOME/zmodules"
+ZI[ZCOMPDUMP_PATH]="$XDG_CACHE_HOME/zcompdump"
+ZI[MAN_DIR]="${ZPFX}/man"
+
 #ZI[COMPINIT_OPTS]=-C
 #ZI[MUTE_WARNINGS]=0
 #ZI[OPTIMIZE_OUT_DISK_ACCESSES]=0
 
 #
-zi_home="${X_ZSH_DATA_HOME}/zpm/zi"
-source "${zi_home}/bin/zi.zsh"
+source "${ZI_HOME}/bin/zi.zsh"
 
 #
 autoload -Uz _zi
 (( ${+_comps} )) && _comps[zi]=_zi
 
-#
+# plugins
 
+# - Completions
+zi ice lucid wait as'completion'
+zi light zsh-users/zsh-completions
+
+zi ice lucid wait has'fzf'
+zi light Aloxaf/fzf-tab
+
+# - Plugins
+zi ice wait lucid atinit"ZI[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay"
+zi light z-shell/F-Sy-H
+
+zi ice wait lucid atload"!_zsh_autosuggest_start"
+zi load zsh-users/zsh-autosuggestions
+
+zi ice wait lucid
+zi load wfxr/forgit
+
+# -----------------------------------------------
 # Fast-syntax-highlighting & autosuggestions
-zi wait lucid for \
-atinit"ZI[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" \
-  z-shell/F-Sy-H \
-atload"!_zsh_autosuggest_start" \
-  zsh-users/zsh-autosuggestions \
-blockf \
-  zsh-users/zsh-completions
 
+# zi wait lucid for \
+# atinit"ZI[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" \
+#   z-shell/F-Sy-H \
+# atload"!_zsh_autosuggest_start" \
+#   zsh-users/zsh-autosuggestions \
+# blockf \
+#   zsh-users/zsh-completions
+# 
 
 # wait'1' plugins
-zi wait"1" lucid for \
-    Aloxaf/fzf-tab
-
+# zi wait"1" lucid for \
+#     wfxr/forgit
+    
 # A few wait'2' plugins
-zi wait"2" lucid for \
-  atinit"forgit_ignore='fgi'" \
-wfxr/forgit
+# zi wait"2" lucid for \
+#   atinit"forgit_ignore='fgi'" \
+# wfxr/forgit
