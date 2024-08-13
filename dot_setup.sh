a1e764048ec47476021c7cbd49a8312799beb2d2
XJ Hsu <xj11400@gmail.com>
2024-08-13 19:15:28 +0800

    (.dotmanager): dot_setup.sh

---
+++ dot_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Check if .dot folder exists in the same directory
+if [ ! -d "./.dotmanager" ]; then
+    echo "cloning .dotmanager"
+    git clone --depth 1 https://github.com/xj11400/.dotmanager.git .dotmanager
+fi
+
+source ./.dotmanager/xdots dot
